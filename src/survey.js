fd7134fce49438360be05bebc7bd7c97cb6085b4
Vandana Keshavamurthy <vandanakeshavamurthy@Vandanas-MacBook-Pro.local>
2020-04-11 21:48:41 -0400

    changed survey.js

--- src/survey.js
+++ src/survey.js
@@ -1,19 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
-const Home = () => {
-    const { classes, url } = props;
-    let [clicks, setClicks] = useState(0);
-
-    const submit = function (event) {
-        console.log(event.currentTarget.getAttribute('data-something'));
-        setClicks(clicks + 1);
-    }
+function survey() {}
     return (
         <React.Fragment>
             <p>Hi there HELLO!</p>
-            <p>click count:{clicks}</p>
-            <button onClick={submit} data-something="Default">
-                Default</button>
 
         </React.Fragment>
 
