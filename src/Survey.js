bd4334a5c5c5084ecfea4dbf650866cf065f8243
Vandana Keshavamurthy <vandanakeshavamurthy@Vandanas-MacBook-Pro.local>
2020-04-12 23:22:35 -0400

    changed a little

--- src/Survey.js
+++ src/Survey.js
@@ -1,17 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
-export default function Survey(props) {
-    const { classes, url } = props;
-    let [clicks, setClicks] = useState(0);
-
-    const doSomething = function (event) {
-        console.log(event.currentTarget.getAttribute('data-something'));
-        setClicks(clicks + 1);
-    }
+export default function Survey() {
     return (
       <div>
         <input type="button" id="greatButton" value="Everything was great!"/>
-        <p className="f-09">Rate everything as a 5.</p>
+        <p>Rate everything as a 5.</p>
       </div>
     );
 }
