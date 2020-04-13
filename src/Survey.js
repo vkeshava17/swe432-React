27115e27a77ef7735af5b8e4484518d278a3b294
Vandana Keshavamurthy <vandanakeshavamurthy@Vandanas-MacBook-Pro.local>
2020-04-12 23:26:43 -0400

    please work

--- src/Survey.js
+++ src/Survey.js
@@ -1,10 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-export default function Survey() {
-    return (
-      <div>
-        <input type="button" id="greatButton" value="Everything was great!"/>
-        <p>Rate everything as a 5.</p>
-      </div>
-    );
+function Survey() {
+    return (<p>Rate everything as a 5.</p>);
 }
