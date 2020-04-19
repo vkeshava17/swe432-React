11c84a1f817ccb417b8da3920fdbc799454cbc2d
Vandana Keshavamurthy <vandanakeshavamurthy@Vandanas-MacBook-Pro.local>
2020-04-19 16:36:44 -0400

    style

--- src/index.js
+++ src/index.js
@@ -215,7 +215,7 @@ class Survey extends React.Component {
 					<p> Additional Comments, Suggestions, and Concerns:</p>
 					<textarea id="textArea" name="Comments" rows="6" cols="40"></textarea>
 				</div>
-			<button type="submit">Submit Answers</button>
+			<button type="submit" style="width: 10%; height: 4%; font-size: 13px;">Submit Answers</button>
 			</div>
 		</form>
 
